8df5ecc84472edc9f18ad09bd0b02c0e4b22a6bb
Bryson LeBlanc <bwl0016@auburn.edu>
2026-03-19 17:14:32 -0500

    toying with getting test data into the view

--- src/weglsite/djrecord/views.py
+++ src/weglsite/djrecord/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import DJ
 
 # Create your views here.
 def index(request):
-    return render(request, "djrecord/index.html")
+    data = DJ.objects.all()
+    print(data)
+    return render(request, "djrecord/index.html", {"data": data})
