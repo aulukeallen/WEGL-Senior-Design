b043adcab38af869aad7ecfd85ae4f55f71a497f
aulukeallen <l_w_allen@outlook.com>
2026-03-20 00:33:57 -0500

    Physical Media - Create and migrate db model, display on page

--- src/weglsite/physicalmedia/views.py
+++ src/weglsite/physicalmedia/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import MediaRecord
 
 # Create your views here.
 def index(request):
-    return render(request, "physicalmedia/index.html")
+
+    entries = MediaRecord.objects.order_by('artist')
+
+    return render(request, "physicalmedia/index.html", {"entries": entries})
