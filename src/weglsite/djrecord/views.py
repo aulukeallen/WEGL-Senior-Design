68e52f6afaefe200d49aedd13bb72d8da0c14776
Bryson LeBlanc <bwl0016@auburn.edu>
2026-03-29 17:03:46 -0500

    search and filter

--- src/weglsite/djrecord/views.py
+++ src/weglsite/djrecord/views.py
@@ -1,13 +1,24 @@
+
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 from .models import DJ
 from .forms import DjInfoForm
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
+    from .forms import DjInfoForm
+    from .forms_search import DJSearchForm
+    form = DJSearchForm(request.GET or None)
     data = DJ.objects.all()
-    print(data)
-    return render(request, "djrecord/index.html", {"data": data})
+    search_query = request.GET.get('search', '').strip()
+    if search_query:
+        data = data.filter(
+            Q(firstName__icontains=search_query) |
+            Q(lastName__icontains=search_query) |
+            Q(email__icontains=search_query)
+        )
+    return render(request, "djrecord/index.html", {"data": data, "form": form})
 
 # # CREATE
 # def create_dj(request):
