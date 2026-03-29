4714a19b399a8239159eff2c22b451cc09881271
Bryson LeBlanc <bwl0016@auburn.edu>
2026-03-29 16:39:54 -0500

    move dj editting to administration page

--- src/weglsite/djrecord/admin.py
+++ src/weglsite/djrecord/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import DJ
 
 # Register your models here.
+@admin.register(DJ)
+class DJAdmin(admin.ModelAdmin):
+    list_display = ('firstName', 'lastName', 'email', 'joinDate')
+    ordering = ('lastName', 'firstName')
+    search_fields = ('firstName', 'lastName', 'email')
--- src/weglsite/djrecord/views.py
+++ src/weglsite/djrecord/views.py
@@ -9,43 +9,43 @@ def index(request):
     print(data)
     return render(request, "djrecord/index.html", {"data": data})
 
-# CREATE
-def create_dj(request):
-    print('create_dj view called')
-    data = DJ.objects.all()
-    if request.method == 'POST':
-        form = DjInfoForm(request.POST)
-        if form.is_valid():
-            form.save()
-            print('success creating DJ')
-            return redirect('djrecord:index')
-        else:
-            print('form errors:', form.errors)
-            return render(request, "djrecord/index.html", {'form': form, 'data': data})
-    # For GET, redirect to index (no separate create page)
-    return redirect('djrecord:index')
+# # CREATE
+# def create_dj(request):
+#     print('create_dj view called')
+#     data = DJ.objects.all()
+#     if request.method == 'POST':
+#         form = DjInfoForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             print('success creating DJ')
+#             return redirect('djrecord:index')
+#         else:
+#             print('form errors:', form.errors)
+#             return render(request, "djrecord/index.html", {'form': form, 'data': data})
+#     # For GET, redirect to index (no separate create page)
+#     return redirect('djrecord:index')
 
-# EDIT
-def edit_dj(request, pk):
-    print('edit_dj view called with pk:', pk)
-    item = get_object_or_404(DJ, pk=pk)
-    data = DJ.objects.all()
-    if request.method == 'POST':
-        form = DjInfoForm(request.POST, instance=item)
-        if form.is_valid():
-            form.save()
-            return redirect('djrecord:index')
-        else:
-            return render(request, "djrecord/index.html", {'form': form, 'data': data})
-    # For GET, redirect to index (no separate edit page)
-    return redirect('djrecord:index')
+# # EDIT
+# def edit_dj(request, pk):
+#     print('edit_dj view called with pk:', pk)
+#     item = get_object_or_404(DJ, pk=pk)
+#     data = DJ.objects.all()
+#     if request.method == 'POST':
+#         form = DjInfoForm(request.POST, instance=item)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('djrecord:index')
+#         else:
+#             return render(request, "djrecord/index.html", {'form': form, 'data': data})
+#     # For GET, redirect to index (no separate edit page)
+#     return redirect('djrecord:index')
 
-# DELETE
-def delete_dj(request, pk):
-    print('delete_dj view called with pk:', pk)
-    item = get_object_or_404(DJ, pk=pk)
-    if request.method == 'POST':
-        item.delete()
-        return redirect('djrecord:index')
-    # For GET, redirect to index (no separate delete page)
-    return redirect('djrecord:index')
+# # DELETE
+# def delete_dj(request, pk):
+#     print('delete_dj view called with pk:', pk)
+#     item = get_object_or_404(DJ, pk=pk)
+#     if request.method == 'POST':
+#         item.delete()
+#         return redirect('djrecord:index')
+#     # For GET, redirect to index (no separate delete page)
+#     return redirect('djrecord:index')
