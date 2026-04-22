895f3a5e83dc4fed0bdf00901bdc5bb4cfdb7c9a
aulukeallen <l_w_allen@outlook.com>
2026-04-22 11:08:40 -0500

    Move secret key and add to gitignore

--- src/weglsite/weglsite/settings.py
+++ src/weglsite/weglsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,12 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a8qv7#vh#&vaq8pyh2&r4gaqw7nls0e^91wpt5^v0eo*_ni+q3'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'lwa0011.pythonanywhere.com',
+    'wegltools.com',
+]
 
 
 # Application definition
