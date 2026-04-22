294203765d9862a7f9a31d8a74e706cc25d02305
aulukeallen <l_w_allen@outlook.com>
2026-04-22 12:09:51 -0500

    Fix static file bug

--- src/weglsite/weglsite/settings.py
+++ src/weglsite/weglsite/settings.py
@@ -140,9 +140,7 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'homepage', 'static'),
-]
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
